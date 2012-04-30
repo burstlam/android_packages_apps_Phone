d76735788a1581aa54dd60cb054a1707935f1d76
burstlam <burstlam@gmail.com>
2012-05-01 00:50:51 +0800

    Revert "Forward-ports CM7 Advanced Phone Settings"
    This reverts commit f665df3210f74403b76277fd1c56a5c052e36f57.
    
    Conflicts:
    
    	res/values-zh-rCN/strings.xml
    	res/values-zh-rTW/strings.xml
    	res/values/strings.xml

--- src/com/android/phone/CallNotifier.java
+++ src/com/android/phone/CallNotifier.java
@@ -47,6 +47,7 @@ import android.text.TextUtils;
 import android.util.EventLog;
 import android.util.Log;
 
+
 /**
  * Phone app module that listens for phone state changes and various other
  * events from the telephony layer, and triggers any resulting UI behavior
@@ -458,9 +459,6 @@ public class CallNotifier extends Handler
         if (PhoneUtils.isRealIncomingCall(state)) {
             startIncomingCallQuery(c);
         } else {
-            if (PhoneUtils.PhoneSettings.vibCallWaiting(mApplication)) {
-                mApplication.vibrate(200,300,500);
-            }
             if (VDBG) log("- starting call waiting tone...");
             if (mCallWaitingTonePlayer == null) {
                 mCallWaitingTonePlayer = new InCallTonePlayer(InCallTonePlayer.TONE_CALL_WAITING);
@@ -806,28 +804,6 @@ public class CallNotifier extends Handler
             }
 
             if (VDBG) log("onPhoneStateChanged: OFF HOOK");
-
-            Call call = PhoneUtils.getCurrentCall(fgPhone);
-            Connection c = PhoneUtils.getConnection(fgPhone, call);
-            if (VDBG) PhoneUtils.dumpCallState(fgPhone);
-            Call.State cstate = call.getState();
-
-            if (cstate == Call.State.ACTIVE && !c.isIncoming()) {
-                long callDurationMsec = c.getDurationMillis();
-                if (VDBG) Log.v(LOG_TAG, "duration is " + callDurationMsec);
-
-                boolean vibOut = PhoneUtils.PhoneSettings.vibOutgoing(mApplication);
-                if (vibOut && callDurationMsec < 200) {
-                    mApplication.vibrate(100,0,0);
-                }
-
-                boolean vib45 = PhoneUtils.PhoneSettings.vibOn45Secs(mApplication);
-                if (vib45) {
-                    callDurationMsec = callDurationMsec % 60000;
-                    mApplication.start45SecondVibration(callDurationMsec);
-                }
-            }
-
             // make sure audio is in in-call mode now
             PhoneUtils.setAudioMode(mCM);
 
@@ -1038,14 +1014,6 @@ public class CallNotifier extends Handler
             // Remove Call waiting timers
             removeMessages(CALLWAITING_CALLERINFO_DISPLAY_DONE);
             removeMessages(CALLWAITING_ADDCALL_DISABLE_TIMEOUT);
-        }
-
-        if (c != null) {
-            boolean vibHangup = PhoneUtils.PhoneSettings.vibHangup(mApplication);
-            if (vibHangup && c.getDurationMillis() > 0) {
-                mApplication.vibrate(50, 100, 50);
-            }
-            mApplication.stopVibrationThread();
         }
 
         // Stop the ringer if it was ringing (for an incoming call that
