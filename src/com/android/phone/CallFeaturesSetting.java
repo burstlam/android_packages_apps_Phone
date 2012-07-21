7036b72de1d107fb685f6bf5dd3790dcccae2cf2
Matanya Elchanani <melchanani@hotmail.com>
2012-07-21 16:23:02 -0400

    Phone: Add Ring Delay feature (port forward)
    This allows the user to set the number of seconds
    before the ringer goes off. This is useful when
    vibrate is enabled to get "vibrate before ring".
    
    Change-Id: If9e72333cc5ce8203bc764e918ad8082312dccb7
    
    Conflicts:
    	src/com/android/phone/CallFeaturesSetting.java
    	src/com/android/phone/Ringer.java
    
    Conflicts:
    	src/com/android/phone/CallFeaturesSetting.java

--- src/com/android/phone/CallFeaturesSetting.java
+++ src/com/android/phone/CallFeaturesSetting.java
@@ -183,6 +183,7 @@ public class CallFeaturesSetting extends PreferenceActivity
     private static final String BUTTON_DTMF_KEY        = "button_dtmf_settings";
     private static final String BUTTON_RETRY_KEY       = "button_auto_retry_key";
     private static final String BUTTON_TTY_KEY         = "button_tty_mode_key";
+    /* package */ static final String BUTTON_RING_DELAY_KEY = "button_ring_delay_key";
     private static final String BUTTON_HAC_KEY         = "button_hac_key";
     private static final String BUTTON_NOISE_SUPPRESSION_KEY = "button_noise_suppression_key";
 
@@ -295,6 +296,7 @@ public class CallFeaturesSetting extends PreferenceActivity
     private CheckBoxPreference mButtonHAC;
     private ListPreference mButtonDTMF;
     private ListPreference mButtonTTY;
+    private ListPreference mButtonRingDelay;
     private CheckBoxPreference mButtonNoiseSuppression;
     private ListPreference mButtonSipCallOptions;
     private ListPreference mVoicemailProviders;
@@ -519,6 +521,8 @@ public class CallFeaturesSetting extends PreferenceActivity
             Settings.System.putInt(mPhone.getContext().getContentResolver(),
                     Settings.System.NOISE_SUPPRESSION, nsp);
             return true;
+        } else if (preference == mButtonRingDelay) {
+            return true;
         } else if (preference == mButtonAutoRetry) {
             android.provider.Settings.Global.putInt(mPhone.getContext().getContentResolver(),
                     android.provider.Settings.Global.CALL_AUTO_RETRY,
@@ -625,6 +629,9 @@ public class CallFeaturesSetting extends PreferenceActivity
                 mChangingVMorFwdDueToProviderChange = true;
                 saveVoiceMailAndForwardingNumber(newProviderKey, newProviderSettings);
             }
+        } else if (preference == mButtonRingDelay) {
+            mButtonRingDelay.setValue((String) objValue);
+            mButtonRingDelay.setSummary(mButtonRingDelay.getEntry());
         } else if (preference == mButtonSipCallOptions) {
             handleSipCallOptionsChange(objValue);
         }
@@ -1554,6 +1561,7 @@ public class CallFeaturesSetting extends PreferenceActivity
         mButtonHAC = (CheckBoxPreference) findPreference(BUTTON_HAC_KEY);
         mButtonTTY = (ListPreference) findPreference(BUTTON_TTY_KEY);
         mButtonNoiseSuppression = (CheckBoxPreference) findPreference(BUTTON_NOISE_SUPPRESSION_KEY);
+        mButtonRingDelay = (ListPreference) findPreference(BUTTON_RING_DELAY_KEY);
         mVoicemailProviders = (ListPreference) findPreference(BUTTON_VOICEMAIL_PROVIDER_KEY);
         if (mVoicemailProviders != null) {
             mVoicemailProviders.setOnPreferenceChangeListener(this);
@@ -1623,6 +1631,15 @@ public class CallFeaturesSetting extends PreferenceActivity
             } else {
                 prefSet.removePreference(mButtonNoiseSuppression);
                 mButtonNoiseSuppression = null;
+            }
+        }
+
+        if (mButtonRingDelay != null) {
+            if (getResources().getBoolean(R.bool.ringdelay_enabled)) {
+                mButtonRingDelay.setOnPreferenceChangeListener(this);
+            } else {
+                prefSet.removePreference(mButtonRingDelay);
+                mButtonRingDelay = null;
             }
         }
 
@@ -1846,6 +1863,13 @@ public class CallFeaturesSetting extends PreferenceActivity
         if (migrateVoicemailVibrationSettingsIfNeeded(prefs)) {
             mVoicemailNotificationVibrate.setChecked(prefs.getBoolean(
                     BUTTON_VOICEMAIL_NOTIFICATION_VIBRATE_KEY, false));
+        }
+
+        if (mButtonRingDelay != null) {
+            CharSequence mPrefEntry = mButtonRingDelay.getEntry();
+            if (mPrefEntry != null) {
+                mButtonRingDelay.setSummary(mPrefEntry);
+            }
         }
 
         lookupRingtoneName();
